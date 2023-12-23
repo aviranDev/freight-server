1e2bf835113737d1fbf097707d51c46ad979a263
Aviran Itzhak <98554893+aviranDev@users.noreply.github.com>
2023-12-23 04:03:13 +0200

    change cron job

--- src/utils/cronJob.ts
+++ src/utils/cronJob.ts
@@ -8,6 +8,32 @@ import { logger } from "../logger/logger";
  */
 export const startCronJob = () => {
   new CronJob(
+    "*/5 * * * *", // Cron schedule: Runs every 5 minutes
+    async () => {
+      // Calculate the expiration date to identify records older than 7 days
+      const expirationDate = new Date();
+      expirationDate.setMinutes(expirationDate.getMinutes() - 20);
+
+      try {
+        // Remove active sessions older than the calculated expirationDate
+        await Session.deleteMany({ lastLogin: { $lte: expirationDate } });
+
+        // Log a debug message indicating successful execution of the cron job
+        logger.debug("Cron job to remove expired refresh tokens ran at: " + new Date().toLocaleString());
+      } catch (error) {
+        // Log an error message if an error occurs during the cron job execution
+        logger.error("Error in Cron job:", error);
+      }
+    },
+    null, // Callback function parameter (null as it's not needed)
+    true, // Start the cron job immediately upon instantiation
+    Intl.DateTimeFormat().resolvedOptions().timeZone // Time zone for the cron job
+  );
+}
+
+/* 
+export const startCronJob = () => {
+  new CronJob(
     "0 * * * *", // Cron schedule: Runs every hour
     async () => {
       // Calculate the expiration date to identify records older than 7 days
@@ -30,5 +56,6 @@ export const startCronJob = () => {
     Intl.DateTimeFormat().resolvedOptions().timeZone // Time zone for the cron job
   );
 }
+*/
 
 export default startCronJob;
